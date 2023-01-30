71c4033c9e33ec9b9935e7589f39d9e10f5df9c9
julianny19 <camachojulianny26@gmail.com>
2023-01-30 18:04:59 +0100

    strings

---
+++ strings.js
@@ -0,0 +1,14 @@
+// tarea string
+let nombre= " julianny";
+let apellido= "camacho ";
+let estudiante= nombre.concat("  ",apellido);
+let estudianteMayus= estudiante.toUpperCase();
+let estudianteMinus= estudianteMayus.toLowerCase();
+let numeroLetras= estudiante.length;
+let letraNombre= nombre.charAt(0);
+let letraApellido= apellido.charAt(6);
+let espacios= estudiante.trim().length;
+let incluido= estudiante.includes("julianny");
+
+
+
