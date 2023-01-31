6d6688e82f48a7dd6453357ba56ca35365eac811
julianny19 <camachojulianny26@gmail.com>
2023-01-31 17:54:50 +0100

    numeros

---
+++ numeros.js
@@ -0,0 +1,7 @@
+let alturaEntero=162;
+let alturaDecimal=1.62;
+let peso=59.8;
+let alturaRedondeada= alturaDecimal.toPrecision(2);
+let pesoRedondeado= peso.toFixed();
+let valorMax=Number.MAX_VALUE;
+let total= valorMax+1 === valorMax;
