49c6eafea308580c90c1c442250344df5603a62f
julianny19 <camachojulianny26@gmail.com>
2023-02-23 18:14:42 +0100

    fechas

---
+++ fechas.js
@@ -0,0 +1,6 @@
+let hoy= new Date();
+let nacimiento= new Date(1996,02,19);
+let comparacion= hoy>nacimiento;
+let dia= nacimiento.getDate();
+let mes=nacimiento.getMonth()+1;
+let anyo= nacimiento.getFullYear();
